a85ea916e130264bf3325c0c32dfcb0c802b29c5
Cody Gagnon <codycodes@users.noreply.github.com>
2025-09-09 20:54:04 -0700

    lint

--- variables.tf
+++ variables.tf
@@ -31,15 +31,15 @@ variable "redirect_uri" {
 }
 
 variable "rotation_days" {
-  type = number
+  type        = number
   description = "Days until secret needs to be recreated. Note: the default of this value is its current maximum value."
   default     = 730
 }
 
 variable "rotation_window_days" {
-  type = number
+  type        = number
   description = "Days to begin recreating secret within if Terraform is run during this time."
-  default = 365
+  default     = 365
 }
 
 variable "custom_permissions" {
