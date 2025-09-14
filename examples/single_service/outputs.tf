3906d6e3159c45fe7ee0cd4e0d35f7e564811a5a
Cody Gagnon <codycodes@users.noreply.github.com>
2025-09-13 21:59:13 -0700

    for ease of use/maintenance we can just for_each everything

--- examples/single_service/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "client_id_and_secret" {
-  sensitive = true
-  value     = { id = module.ms365_hass.client_id, secret = module.ms365_hass.client_secret }
-}
-
-output "disclaimer" {
-  value = "please run 'terraform output client_id_and_secret' to fetch its value"
-}
